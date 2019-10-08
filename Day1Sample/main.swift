29b1776dff8527f7445f7871dbef79e581bc759c
NitinJaswal <nathanjaswal@gmail.com>
2019-10-07 20:29:16 -0400

    [Nitin] First Commit

--- Day1Sample/main.swift
+++ Day1Sample/main.swift
@@ -8,5 +8,72 @@
 
 import Foundation
 
-print("Hello, World!")
+//import UIKit
+
+let 😁 = "Tento";
+
+var str = "1"
+
+let j = 10
+
+var emj = "😁"
+
+var _ = 100
+
+let c = String(format: "Sum : \(Int(str)! + j) \(emj) \(😁)" )
+print(c)
+
+
+//readLine(strippingNewline: 5)
+
+// ---- Tuples ----
+var x1 = (1, "nitin", "Jas")
+print(x1.0)
+
+var x2 = (studentId: 2, collegeName: "Lambton", city: "Toronto")
+print(x2.studentId)
+
+var x3 = (collegeName: x2.collegeName, city: x2.city)
+print(x3)
+
+var (studentId, collegeName, city) = x2
+print(studentId)
+
+
+
+//----- Operator ---------
+
+// 1.
+var k: String!
+
+print(k ?? "S")
+
+// 2. Range Operator
+
+//for index in 0..<10{
+//    print(index)
+//}
+
+for index in stride(from: 9, to: 0, by: -1){
+    print(index)
+}
+
+//for index in 0...10{
+//    print(index)
+//}
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+
+for i in names[...3] {
+    print(i)
+}
+
+for i in names[2...3] {
+    print(i)
+}
+
+
+
+
+
 
